e44433fc691b6c207f34a47f36763cf0020f1b61
mingchaoyan <mingchaoyan@gmail.com>
2016-07-03 22:25:59 +0800

    add source

---
+++ MatrixTransform/Triangle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace MatrixTransform
+{
+    class Triangle
+    {
+        PointF A, B, C;
+        public Triangle(PointF A, PointF B, PointF C) {
+            this.A = A;
+            this.B = B;
+            this.C = C;
+        }
+
+        public void Draw(Graphics g) {
+            Pen pen = new Pen(Color.Red, 2);
+            g.DrawLine(pen, A, B);
+            g.DrawLine(pen, B, C);
+            g.DrawLine(pen, C, A);
+        }
+
+        public void Rotate(int degree) {
+            A = MatrixMul(A, degree);
+            B = MatrixMul(B, degree);
+            C = MatrixMul(C, degree);
+        }
+
+        private PointF MatrixMul(PointF A, int degree) { 
+            float angle = (float)(degree / 360.0f * Math.PI);
+            float newX = (float)(A.X * Math.Cos(angle) - A.Y * Math.Sin(angle));
+            float newY = (float) (A.X * Math.Sin(angle) + A.Y * Math.Cos(angle));
+            return new PointF(newX, newY);
+        }
+    }
+}
